e87f28411fa6a6bc73f20620f508299a1840575f
Javier Canino Vázquez <jcaninov@gmail.com>
2017-01-15 19:38:57 +0100

    fix rfid.js validation

--- services/controllers/rfid.js
+++ services/controllers/rfid.js
@@ -13,10 +13,19 @@ module.exports = function (eventEmitter){
 
 	router.get('/get-rfid', function (req, res) {
         var fcontent = fs.readFileSync('rfid.txt', 'utf8');
-	var fparsed = fcontent.split(/(\n)/);
-	rfid = fparsed[fparsed.length-1];
+        var fparsed = fcontent.split(/(\n)/).reverse();
+        for (var i = 0; i < fparsed.length; i++) {
+            if (fparsed[i] != "" && fparsed[i] != "\n" && fparsed[i].indexOf('Rfid') == -1) {
+                rfid = fparsed[i];
+                break;
+            }
+        }
+	    
         res.send(rfid);
     });
+    
+    function getRfid(fcontent) {
+    }
 
 	return router;
 };
